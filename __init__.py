e4e19eb0fa2b53c311718e40257a28628e563957
aditid <aditid@vmware.com>
2022-07-19 16:12:05 +0530

    Redirect url

--- __init__.py
+++ __init__.py
@@ -21,6 +21,14 @@ def createShortenedUrl():
     msg = "http://127.0.0.1:5000/"+contents[0] if contents[1]==200 else contents[0]
     return (msg,contents[1])
 
+@app.route("/short/<shortUrl>/")
+def redirectToLongUrl(shortUrl):
+    str long_url = get_longurl(shortUrl)
+    # Call to get the long url from the db
+    if len(long_url)==0
+        return redirect("/notfound/error/")
+    else : 
+        return redirect(url)
 
 if __name__ == "__main__":
     app.run(debug = True)
